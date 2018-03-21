fd9d6ae245636ea473513658a2417344c5cf27da
WANG ZEHAO <moyutianque@outlook.com>
2018-03-21 09:32:46 +0800

    Add files via upload

---
+++ workplace/train/feature_extraction.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python  
+# -*- coding: utf-8 -*- 
+import os
+import dlib
+import cv2
+import numpy as np
+from imutils import face_utils
+import my_utils
+import csv
+
+file_name = "..\\shape_predictor_68_face_landmarks.dat"
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(file_name)
+
+#dir = '.\\instead_laugh\\'
+dir = '.\\hello\\'
+output_csv = "1.csv"
+with open(output_csv ,"w",newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["ft1","ft2","ft3","ft4","ft5","ft6","ft7","ft8","ft9","ft10"])  ## [INFO] revise attention 
+    for root,dirs,files in os.walk(dir):
+        for file in files:
+            srcImg = cv2.imread(str(dir)+file)
+            rects = detector(srcImg, 0)
+            for rect in rects:
+                shape = predictor(srcImg, rect)
+                shape = face_utils.shape_to_np(shape)
+                # draw the face
+                (x, y, w, h) = face_utils.rect_to_bb(rect)
+                #cv2.rectangle(srcImg, (x, y), (x + w, y + h), (255, 255, 255), 2)
+                # draw the landmarks
+                for (x,y) in shape:
+                    cv2.circle(srcImg, (x,y), 1, (255,144,30), -1)
+            
+                writer.writerow(my_utils.extract_features(shape))
+                
+            
+            # [INFO] comment it down after testing
+            cv2.namedWindow("%s" % {str(file)},cv2.WINDOW_NORMAL)
+            cv2.resizeWindow("%s" % {str(file)}, 640, 480)
+            cv2.imshow("%s" % {str(file)}, srcImg)      
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+    
---
+++ workplace/train/train.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python  
+# -*- coding: utf-8 -*- 
+import os
+import dlib
+import cv2
+import time
+import numpy as np
+from imutils import face_utils
+import my_utils
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
+from sklearn.model_selection import cross_val_score
+
+
+def main():
+    time1 = time.localtime(time.time())
+    l_data = 'laugh.csv'
+    il_data = 'instead_laugh.csv'
+    m_path = '.\model\learned_model%s.pkl'%(str(time1.tm_mon)+str(time1.tm_mday))
+    df1 = pd.read_csv(l_data)
+    df2 = pd.read_csv(il_data)
+    #print(df2.info)
+    pos = df1.values
+    neg = df2.values
+    
+    X = np.r_[pos,neg] # c_ and r_ is used to concatenate array
+    Y = [1] * pos.shape[0] + [0] * neg.shape[0]
+    
+    if os.path.exists(m_path):
+        clf = joblib.load(m_path) 
+    else:
+        print("[INFO] start training")
+        clf = RandomForestClassifier(n_estimators=100,min_samples_split=2)
+        clf.fit(X,Y)
+        joblib.dump(clf, m_path)
+        
+    # test1
+    print("[INFO] running test1")
+    scores = cross_val_score(clf, X, Y, cv=5, scoring='accuracy')
+    print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(),'Random Forest'))
+    
+    #test2 webcam
+    print("[INFO] running test2")
+    x_test = np.asarray([[1.838462802,1.850380919,1.813466246,1.586179729,0.77206562,0.826866573,2.618314331,1.059522838,1.142230869,1.169812414]])
+    print(clf.predict(x_test))
+    
+if __name__ == '__main__':
+    main()
+
+    
